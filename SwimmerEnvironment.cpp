b183350dd08a4b95776bce5e154b7165683fcf1e
Léon Zheng <leonpapanoel@gmail.com>
2019-04-25 14:48:08 +0900

    Finshed computer accelerations. Need to test

--- SwimmerEnvironment.cpp
+++ SwimmerEnvironment.cpp
@@ -41,7 +41,7 @@ const reward_observation_terminal_t *env_step(const action_t *this_action)
 		assert(abs(this_action->doubleArray[i]) < max_u);
 	}
 
-	updateState(this_observation, this_action->doubleArray);
+	updateState(this_observation, this_action);
 	this_reward_observation.observation = &this_observation;
 	this_reward_observation.reward = calculate_reward(this_observation);
 	this_reward_observation.terminal = check_terminal(this_observation);
@@ -71,10 +71,122 @@ const char* env_message(const char * message)
 /*
 	Helper methods
 */
-void updateState(observation_t& state, const double* torques)
+void computeAccelerations(const double* torque, const Vector2d p_head, const Vector2d v_head, const double* p_angle, const double* v_angle, 
+							double* a_angle, Vector2d& a_head)
 {
+	// Computes direction unit vectors
+	Vector2d p_i[n_seg];
+	Vector2d n_i[n_seg];
+	for(int i=0; i<n_seg; i++){
+		p_i[i] = Vector2d(cos(p_angle[i]), sin(p_angle[i]));
+		n_i[i] = Vector2d(-sin(p_angle[i]), cos(p_angle[i]));
+	}
 
+	// Compute point's positions and speed
+	Vector2d p_points[n_seg+1];
+	Vector2d v_points[n_seg+1];
+	p_points[0] = p_head;
+	v_points[0] = v_head;
+	for(int i=1; i<n_seg+1; i++){
+		p_points[i] = p_points[i-1] + l_i*p_i[i-1];
+		v_points[i] = v_points[i-1] + l_i*v_angle[i-1]*n_i[i-1];
+	}
+
+	// And also for the mass centers
+	Vector2d p_center[n_seg];
+	Vector2d v_center[n_seg];
+	for(int i=0; i<n_seg; i++){
+		p_center[i] = (p_points[i]+p_points[i+1])/2;
+		v_center[i] = (v_points[i]+v_points[i+1])/2;
+	}
+
+	// Compute friction forces and torques
+	Vector2d F_friction[n_seg];
+	double M_friction[n_seg];
+	for(int i=0; i++; i<n_seg){
+		F_friction[i] = -k*l_i*v_center[i].dot(n_i[i])*n_i[i];
+		M_friction[i] = -k*v_angle[i]*pow(l_i,3)/12;
+	}
+
+	// Matrix and vector of the linear system
+	MatrixXd A = MatrixXd::Zero(5*n_seg+1, 5*n_seg+1);
+	VectorXd B = VectorXd::Zero(5*n_seg+1);
+
+	// Dynamic equations: lines 0 to n_seg-1
+	for(int i=1; i<n_seg+1; i++){ // angles..
+		A(i-1, i-1) = m_i*l_i/12;
+		A(i-1, n_seg + 2*i) = l_i/2*sin(p_angle[i-1]); // f_(i, x)
+		A(i-1, n_seg + 2*(i+1)) = l_i/2*sin(p_angle[i-1]); // f_(i+1, x)
+		A(i-1, n_seg + 2*i + 1) = -l_i/2*cos(p_angle[i-1]); // f_(i, y)
+		A(i-1, n_seg + 2*(i+1) + 1) = -l_i/2*cos(p_angle[i-1]); // f_(i+1, y)
+		B(i-1) = M_friction[i-1]-torque[i-1];
+		if(i-2>=0)	B(i-1) += torque[i-2];
+	}
+
+	// Equations on f_i: lines n_seg to 3*n_seg+3
+	// lines n_seg to n_seg+1
+	A(n_seg, n_seg) = 1;
+	A(n_seg+1, n_seg+1) = 1;
+	// lines n_seg+2 to 3*n_seg+1
+	for(int i=1; i<n_seg+1; i++){
+		// Equation on x direction
+		A(n_seg+2 + 2*(i-1), n_seg + 2*(i-1)) = 1; //f_(i-1,x)
+		A(n_seg+2 + 2*(i-1), n_seg + 2*i) = -1; //f_(i,x)
+		A(n_seg+2 + 2*(i-1), 3*n_seg + 2*i) = m_i; //G.._(i,x)
+		B(n_seg+2 + 2*(i-1)) = F_friction[i-1](0);
+		// Equation on y direction
+		A(n_seg+2 + 2*(i-1) + 1, n_seg + 2*(i-1) + 1) = 1; //f_(i-1,y)
+		A(n_seg+2 + 2*(i-1) + 1, n_seg + 2*i + 1) = -1; //f_(i,y)
+		A(n_seg+2 + 2*(i-1) + 1, 3*n_seg + 2*i + 1) = m_i; //G.._(i,y)
+		B(n_seg+2 + 2*(i-1) + 1) = F_friction[i-1](1);
+	}
+	// lines 3*n_seg+2 to 3*n_seg+3
+	A(3*n_seg+1, 3*n_seg) = 1;
+	A(3*n_seg+2, 3*n_seg+1) = 1;
+
+	// Equations on G.._i: lines 3*n_seg+4 to 5*n_seg+1
+	for(int i=1; i<n_seg; i++){
+		// Equation on x direction
+		A(3*n_seg+4 + 2*(i-1), 3*n_seg+2*i) = 1; //G.._(i,x)
+		A(3*n_seg+4 + 2*(i-1), 3*n_seg+2*(i+1)) = -1; //G.._(i+1,x)
+		A(3*n_seg+4 + 2*(i-1), i-1) = -l_i/2*sin(p_angle[i-1]); //theta.._i
+		A(3*n_seg+4 + 2*(i-1), i) = -l_i/2*sin(p_angle[i]); //theta.._(i+1)
+		B(3*n_seg+4 + 2*(i-1)) = l_i/2*(cos(p_angle[i-1])*pow(v_angle[i-1],2) + cos(p_angle[i])*pow(v_angle[i],2));
+		// Equation on y direction
+		A(3*n_seg+4 + 2*(i-1) + 1, 3*n_seg+2*i+1) = 1; //G.._(i,y)
+		A(3*n_seg+4 + 2*(i-1) + 1, 3*n_seg+2*(i+1)+1) = -1; //G.._(i+1,y)
+		A(3*n_seg+4 + 2*(i-1) + 1, i-1) = l_i/2*cos(p_angle[i-1]); //theta.._i
+		A(3*n_seg+4 + 2*(i-1) + 1, i) = l_i/2*cos(p_angle[i]); //theta.._(i+1)
+		B(3*n_seg+4 + 2*(i-1) + 1) = l_i/2*(sin(p_angle[i-1])*pow(v_angle[i-1],2) + sin(p_angle[i])*pow(v_angle[i],2));
+	}
+
+	// Invert matrix, obtain solution, extract second derivatives
+	VectorXd X = A.colPivHouseholderQr().solve(B);
+	for(int i=0; i<n_seg; i++){
+		a_angle[i] = X(i);
+	}
+	a_head = Vector2d(X(3*n_seg+2), X(3*n_seg+3)) - l_i/2*p_i[0];
 }
+
+
+void updateState(observation_t& state, const action_t* action)
+{
+	// Extract the informations
+	const double* torque = action->doubleArray;
+	const Vector2d p_head(state.doubleArray[0], state.doubleArray[1]);
+	const Vector2d v_head(state.doubleArray[2], state.doubleArray[3]);
+	const double* p_angle = &state.doubleArray[4];
+	const double* v_angle = &state.doubleArray[4 + n_seg];
+
+	// Compute accelerations
+	double a_angle[n_seg];
+	Vector2d a_head;
+	computeAccelerations(torque, p_head, v_head, p_angle, v_angle, a_angle, a_head);
+
+	// Semi-implicit Euler
+	
+}
+
 
 double calculate_reward(const observation_t& state)
 {
--- SwimmerEnvironment.h
+++ SwimmerEnvironment.h
@@ -8,11 +8,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h> /*assert*/
+#include "Eigen/Dense" // Eigen library
 // env_ function prototypes types 
 #include <rlglue/Environment_common.h>	  
 // helpful functions for allocating structs and cleaning them up 
 #include <rlglue/utils/C/RLStruct_util.h>   
 
+using namespace Eigen;
 
 // Global variables for RLGlue methods
 static observation_t this_observation;
@@ -24,7 +26,10 @@ static int default_start_state = 1;
 // Parameters
 // TODO put the parameters has an input of the file and don't recompile at each time
 static int n_seg = 3;
-static double max_u = 1;
+static double max_u = 5;
+static double l_i = 1;
+static double k = 10;
+static double m_i = 1;
 
 // Methods
 const char* env_init();
@@ -33,7 +38,9 @@ const reward_observation_terminal_t *env_step(const action_t *this_action);
 void env_cleanup();
 const char* env_message(const char * message);
 
-void updateState(observation_t& state, const double* torques);
+void computeAccelerations(const double* torque, const Vector2d p_head, const Vector2d v_head, const double* p_angle, const double* v_angle, 
+							double* a_angle, Vector2d& a_head);
+void updateState(observation_t& state, const action_t* action);
 double calculate_reward(const observation_t& state);
 int check_terminal(const observation_t& state);
 
