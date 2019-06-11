5d7688b95b68d45458641a15af2570a465b7e42b
Léon Zheng <leonpapanoel@gmail.com>
2019-06-11 10:59:09 +0900

    update Swimmer gym: only ADDME

--- gym-swimmer/gym_swimmer/__init__.py
+++
@@ -1,6 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id='Leon-swimmer-v1',
-    entry_point='gym_swimmer.envs:SwimmerEnv'
-)
--- gym-swimmer/gym_swimmer/envs/__init__.py
+++
@@ -1,1 +0,0 @@
-from gym_swimmer.envs.remy_swimmer_env import SwimmerEnv
