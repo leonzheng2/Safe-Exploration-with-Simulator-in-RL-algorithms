7e441ab78466e81b604fa055800256f9bd46445d
Leon Zheng <leonpapanoel@gmail.com>
2019-07-11 14:59:28 +0900

    Add Bounded Easy Parameterized LQR

--- envs/gym_lqr/lqr_env.py
+++ envs/gym_lqr/lqr_env.py
@@ -45,3 +45,25 @@ class EasyParamLinearQuadReg(LinearQuadReg):
         super().__init__(A, B, Q, R)
         self.op_norm_der_A = 1
         self.op_norm_der_B = 1
+
+
+class BoundedEasyLinearQuadReg(EasyParamLinearQuadReg):
+
+    def __init__(self, theta, max_s, max_a):
+        super().__init__(theta)
+        self.max_s = max_s
+        self.max_a = max_a
+
+    def reset_inbound(self, x: np.ndarray, M):
+        for i in range(len(x)):
+            if abs(x[i]) > M:
+                x[i] = abs(x[i]) / x[i] * M
+        return x
+
+    def step(self, action: np.ndarray):
+        action = np.array(action)
+        obs = self.A @ self.state + self.B @ action
+        obs = self.reset_inbound(obs, self.max_s)
+        self.state = obs
+        rew = - (self.state.transpose() @ self.Q @ self.state + action.transpose() @ self.R @ action)
+        return obs, rew, False, {}
